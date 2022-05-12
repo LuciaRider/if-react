c6700148d9c63b3521d1879933fa9d12ef000584
Valentina Kozak <kozak_val@mail.ru>
2022-05-12 22:52:04 +0300

    make changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { devToolsEnhancer } from 'redux-devtools-extension';
 import { userAuthReducer, initialUserData } from '../ducks/reducers.js';
 
 export default createStore(
-    initialUserData,
     userAuthReducer,
+    initialUserData,
     devToolsEnhancer(),
 );
